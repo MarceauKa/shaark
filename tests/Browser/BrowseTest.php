f067edcd864fb805b85c7a48539e8d03a0b8afcb
MarceauKa <marceau@casals.fr>
2019-08-24 02:31:46 +0200

    :ok_hand: Tests: link view

--- tests/Browser/BrowseTest.php
+++ tests/Browser/BrowseTest.php
@@ -4,9 +4,9 @@ namespace Tests\Browser;
 
 use App\Link;
 use App\User;
-use Tests\DuskTestCase;
-use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
 
 class BrowseTest extends DuskTestCase
 {
@@ -29,7 +29,30 @@ class BrowseTest extends DuskTestCase
                 ->visit('/')
                 ->assertSee($user->name)
                 ->assertSee($link1->title)
-                ->assertSee($link2->title);
+                ->assertSee($link2->title)
+                ->logout()
+                ->assertGuest();
+        });
+    }
+
+    /** @test */
+    public function it_test_link_view()
+    {
+        $user = factory(User::class)->create();
+        $link1 = factory(Link::class)->create();
+        $link2 = factory(Link::class)->state('private')->create();
+
+        $this->browse(function (Browser $browser) use ($link1, $link2, $user) {
+            $browser
+                ->assertGuest()
+                ->visitRoute('link.view', [$link1->hash_id])
+                ->assertSee($link1->title)
+                ->visitRoute('link.view', [$link2->hash_id])
+                ->assertSee('404')
+                ->loginAs(1)
+                ->visitRoute('link.view', [$link2->hash_id])
+                ->assertSee($link2->title)
+                ->logout();
         });
     }
 }
