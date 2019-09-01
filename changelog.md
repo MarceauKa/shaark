d5521bf5cad04db071f382dabe03e334376b7d1a
MarceauKa <marceau@casals.fr>
2019-09-01 20:51:00 +0200

    :bug: Default settings now use the correct locale

---
+++ changelog.md
@@ -0,0 +1,9 @@
+# Unreleased
+
+## Fixes
+
+- Default settings now use the correct locale
+
+# 1.0.0
+
+- Initial release
--- config/shaarli.php
+++ config/shaarli.php
@@ -2,7 +2,7 @@
 
 return [
     'name' => env('APP_NAME'),
-    'locale' => env('APP_LOCALE'),
+    'locale' => env('APP_LANG'),
     'is_private' => false,
     'is_dark' => false,
 ];
