c40d88eab1ba339c6f8ef92c862ad939c954ec55
MarceauKa <marceau@casals.fr>
2019-09-30 13:48:54 +0200

    :wrench: Bump version

--- app/Services/Shaarli/Shaarli.php
+++ app/Services/Shaarli/Shaarli.php
@@ -15,7 +15,7 @@ use Spatie\Valuestore\Valuestore;
 class Shaarli
 {
     /** @var string VERSION */
-    public const VERSION = '1.2.8';
+    public const VERSION = '1.2.9';
     /** @var Application $app */
     protected $app;
     /** @var Valuestore $settings */
