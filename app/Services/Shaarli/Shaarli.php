eba405b65f1d0db6d3bd8be58323fb305d2a8c78
MarceauKa <marceau@casals.fr>
2019-10-14 16:10:41 +0200

    :bookmark: 1.2.16

--- app/Services/Shaarli/Shaarli.php
+++ app/Services/Shaarli/Shaarli.php
@@ -15,7 +15,7 @@ use Spatie\Valuestore\Valuestore;
 class Shaarli
 {
     /** @var string VERSION */
-    public const VERSION = '1.2.15';
+    public const VERSION = '1.2.16';
     /** @var Application $app */
     protected $app;
     /** @var Valuestore $settings */
