45449418137895ebefe7b8e2e3fc75eb2a652e39
MarceauKa <marceau@casals.fr>
2019-10-27 19:01:42 +0100

    :globe_with_meridians: Missing Export translations

--- resources/views/manage/export.blade.php
+++ resources/views/manage/export.blade.php
@@ -7,20 +7,13 @@
             <div class="card-header">{{ __('Export') }}</div>
 
             <div class="card-body">
-                <p class="card-text">
-                    Vous pouvez exporter vos <strong>liens</strong>, <strong>stories</strong> et <strong>coffres</strong>.
-                    L'export se fait au format <code>.xslx</code> ou <code>.csv</code>.
-                </p>
-            </div>
-
-            <div class="card-footer">
                 <form method="POST" action="{{ route('manage.export') }}" method="POST">
                     @csrf
 
                     <div class="row">
                         <div class="col-12 col-md-6">
                             <div class="form-group">
-                                <label for="type">Source</label>
+                                <label for="type">{{ __('Source') }}</label>
                                 <select name="type" id="type" class="form-control custom-select">
                                     <option value="links">{{ __('Links') }}</option>
                                     <option value="stories">{{ __('Stories') }}</option>
@@ -31,7 +24,7 @@
 
                         <div class="col-12 col-md-6">
                             <div class="form-group">
-                                <label for="format">Format</label>
+                                <label for="format">{{ __('Format') }}</label>
                                 <select name="format" id="format" class="form-control custom-select">
                                     <option value="xlsx">.xlsx</option>
                                     <option value="csv">.csv</option>
