8f99f73fa839ff4c84cc8f686addf7c2619fe0d3
MarceauKa <marceau@casals.fr>
2019-10-30 22:25:05 +0100

    :bug: handleCustomIcon

--- app/Services/Shaarli/Concerns/HandleCustomSettings.php
+++ app/Services/Shaarli/Concerns/HandleCustomSettings.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Storage;
  */
 trait HandleCustomSettings
 {
-    public function handleCustomIcon(UploadedFile $value): void
+    public function handleCustomIcon(?UploadedFile $value): void
     {
         if (empty($value)) {
             return;
