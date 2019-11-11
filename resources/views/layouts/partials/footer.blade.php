26e892b545255904b4d39accdd7badbb95237b9f
MarceauKa <marceau@casals.fr>
2019-11-11 13:17:53 +0100

    :wrench: Link to source with _blank target

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,7 +2,7 @@
     <p class="text-center mb-0">
         {{ app('shaarli')->getName() }}
         &mdash; v{{ app('shaarli')::VERSION }}
-        &mdash; <a href="https://github.com/MarceauKa/laravel-shaarli">{{ __('Source code') }}</a>
+        &mdash; <a href="https://github.com/MarceauKa/laravel-shaarli" target="_blank">{{ __('Source code') }}</a>
         @can('restricted')
             &mdash; <a href="{{ route('feed', 'rss') }}">{{ __('RSS Feed') }}</a>
             &mdash; <a href="{{ route('feed', 'atom') }}">{{ __('Atom Feed') }}</a>
