55ffd89cadb49992613339b6cfd77d488709dfe8
MarceauKa <marceau@casals.fr>
2019-12-08 21:14:23 +0100

    :bookmark: 1.2.35

--- app/Services/Shaark/Shaark.php
+++ app/Services/Shaark/Shaark.php
@@ -19,7 +19,7 @@ class Shaark
         HandleCustomSettings;
 
     /** @var string VERSION */
-    public const VERSION = '1.2.34';
+    public const VERSION = '1.2.35';
     /** @var Application $app */
     protected $app;
 
