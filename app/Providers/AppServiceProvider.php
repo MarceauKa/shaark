0cad1d775cf434c6018ef35e0372e956b41a174e
MarceauKa <marceau@casals.fr>
2020-01-24 15:21:13 +0100

    :wrench: AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,64 +13,119 @@ use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        if ($this->app->environment('production')) {
-            $this->app['request']->server->set('HTTPS', true);
-        }
-
-        $this->app->singleton(Shaark::class, function ($app) {
-            return new Shaark($app);
-        });
-
-        $this->app->alias(Shaark::class, 'shaark');
-
-        $this->app->singleton(Hashid::class, function ($app) {
-            return new Hashid($app['config']->get('shaark.hashids'));
-        });
-
-        $this->app->alias(Hashid::class, 'hashid');
+        $this->registerHttps();
+        $this->registerContainer();
     }
 
     public function boot()
     {
+        $this->bootLocale();
+        $this->bootViewComposer();
+        $this->bootCustomValidation();
+    }
+
+    protected function registerHttps(): self
+    {
+        $url = $this->app['config']->get('app.url');
+
+        if (Str::contains($url, 'https')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
+
+        return $this;
+    }
+
+    protected function registerContainer(): self
+    {
+        $this->app->singleton(
+            Shaark::class,
+            function ($app) {
+                return new Shaark($app);
+            }
+        );
+
+        $this->app->alias(Shaark::class, 'shaark');
+
+        $this->app->singleton(
+            Hashid::class,
+            function ($app) {
+                return new Hashid($app['config']->get('shaark.hashids'));
+            }
+        );
+
+        $this->app->alias(Hashid::class, 'hashid');
+
+        return $this;
+    }
+
+    protected function bootLocale(): self
+    {
         $this->app->setLocale(app('shaark')->getLocale());
 
-        View::composer('layouts.partials.scripts', function (\Illuminate\View\View $view) {
-            $locale = config('app.locale');
+        return $this;
+    }
 
-            if ($locale !== 'en') {
-                $path = resource_path(sprintf('lang/%s.json', $locale));
+    protected function bootViewComposer(): self
+    {
+        View::composer(
+            'layouts.partials.scripts',
+            function (\Illuminate\View\View $view) {
+                $locale = config('app.locale');
 
-                try {
-                    $view->with('lang', json_decode(file_get_contents($path)));
-                } catch (\Exception $e) {
-                    throw new \RuntimeException('Unable to load i18n file.');
+                if ($locale !== 'en') {
+                    $path = resource_path(sprintf('lang/%s.json', $locale));
+
+                    try {
+                        $view->with('lang', json_decode(file_get_contents($path)));
+                    } catch (\Exception $e) {
+                        throw new \RuntimeException('Unable to load i18n file.');
+                    }
+                } else {
+                    $view->with('lang', []);
                 }
-            } else {
-                $view->with('lang', []);
             }
-        });
+        );
 
-        View::composer('layouts.partials.footer', function (\Illuminate\View\View $view) {
-            $version = Cache::remember('version', Carbon::now()->addDay(), function () {
-                $checker = UpdateChecker::check();
+        View::composer(
+            'layouts.partials.footer',
+            function (\Illuminate\View\View $view) {
+                $version = Cache::remember(
+                    'version',
+                    Carbon::now()->addDay(),
+                    function () {
+                        $checker = UpdateChecker::check();
 
-                return $checker->latest;
-            });
+                        return $checker->latest;
+                    }
+                );
 
-            $view->with('version', json_encode($version));
-        });
+                $view->with('version', json_encode($version));
+            }
+        );
 
-        Validator::extend('current_password', function ($attribute, $value, $parameters, $validator) {
-            return Hash::check($value, Auth::user()->getAuthPassword());
-        });
+        return $this;
+    }
 
-        Validator::extend('temp_image', function ($attribute, $value, $parameters, $validator) {
-            return Storage::exists($value);
-        });
+    protected function bootCustomValidation(): self
+    {
+        Validator::extend(
+            'current_password',
+            function ($attribute, $value, $parameters, $validator) {
+                return Hash::check($value, Auth::user()->getAuthPassword());
+            }
+        );
+
+        Validator::extend(
+            'temp_image',
+            function ($attribute, $value, $parameters, $validator) {
+                return Storage::exists($value);
+            }
+        );
     }
 }
