432d043f6509aeb9d19dc403e6438aa534873f60
Tom Janssen <dododedodonl@thor.edu>
2020-04-09 18:21:02 +0200

    add dutch

--- config/shaark.php
+++ config/shaark.php
@@ -16,7 +16,7 @@ return [
         ],
         'locale' => [
             'default' => env('APP_LANG'),
-            'rules' => ['required', 'in:fr,en,de,ja']
+            'rules' => ['required', 'in:fr,en,de,ja,nl']
         ],
         'is_private' => [
             'default' => false,
